581c1bc4aae3d1d8a532c151c750af52331da6c6
axlwy <axlwy@192.168.0.211>
2021-03-06 18:29:33 +0100

    eerste versie dot, niet getest want square nog niet af
    (mogelijk nog rep exposure)

--- pacman/src/pacman/Dot.java
+++ pacman/src/pacman/Dot.java
@@ -2,11 +2,30 @@ package pacman;
 
 /**
  * Each instance of this class represents a dot (a piece of food for Pac-Man) in a Pac-Man maze.
+ * @immutable
  */
 public class Dot {
 	
-	public Square getSquare() { throw new RuntimeException("Not yet implemented"); }
+	/**
+	 * @representationObject
+	 * this variable represents the square that this dot is on
+	 */
+	private Square squareOfDot;
 	
-	public Dot(Square square) { throw new RuntimeException("Not yet implemented"); }
+	/**
+	 * Returns the square that this dot is on.
+	 */
+	public Square getSquare() { 
+		return squareOfDot; 
+		}
+	
+	/**
+	 * Initializes a dot at the given square.
+	 * @post this dot's square equals the given square
+	 *   | getSquare() == square
+	 */
+	public Dot(Square square) {
+		squareOfDot = square; 
+		}
 
 }
