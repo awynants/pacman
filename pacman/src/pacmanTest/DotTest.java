d62b4764e63bd45188651b6ab909510f349d505e
axlwy <axlwy@192.168.0.211>
2021-03-10 18:23:14 +0100

    dotTest gemaakt en betere structuur MazeMapTest

---
+++ pacman/src/pacmanTest/DotTest.java
@@ -0,0 +1,28 @@
+package pacmanTest;
+
+import org.junit.jupiter.api.Test;
+
+import pacman.MazeMap;
+import pacman.Square;
+import pacman.Dot;
+
+class DotTest {
+
+	@Test
+	void test() {
+		int num = 2;
+		MazeMap testMazemap = new MazeMap(3, 5, new boolean[] {false, true, true, false, true, true, true, false, true, false, true, true, true, false, true});
+		Square testSquare = Square.of(testMazemap, 1, num);
+		Dot testDot = new Dot(testSquare);
+		MazeMap testMazemapcopy = new MazeMap(3, 5, new boolean[] {false, true, true, false, true, true, true, false, true, false, true, true, true, false, true});
+		Square testSquarecopy = Square.of(testMazemapcopy, 1, num);
+		
+		//test of getSquare()
+		assert testDot.getSquare() == testSquare;
+		assert testDot.getSquare() != testSquarecopy;
+		num += 1;
+		assert testDot.getSquare() == testSquare;
+		assert testDot.getSquare() != testSquarecopy;
+	}
+
+}
